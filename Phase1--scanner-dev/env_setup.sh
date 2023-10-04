50edd4474680ca9281594a88c80d9c5806471451
CaryRandazzo <randazzocary@gmail.com>
2023-10-03 20:01:28 -0500

    updated installer file to include build software

--- Phase1--scanner-dev/env_setup.sh
+++ Phase1--scanner-dev/env_setup.sh
@@ -1,1 +1,14 @@
+# Install the flex scanner generator
 sudo apt-get install flex -y
+
+# Install a c compiler
+# sudo apt install gccG
+
+# Alternatively, install other tools such as
+# - GNU C compiler
+# - libc6-dev  (header files for GNU C Library, glibc)
+# - g++  (the GNU c++ compiler)
+# - make (build tool for medium/large software packages)
+# - dpkg-dev (build a package in .deb packaging format used by debian and ubuntu)
+# using ...
+sudo apt install build-essential
